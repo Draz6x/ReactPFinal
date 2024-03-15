39fbb31f83556cce169b1b2cbeb1c67dc37dc524
Jorge Caro <informatica.altosaludchile@gmail.com>
2024-03-15 01:07:05 -0300

    final commit2

--- src/Componentes/Checkout/Checkout.jsx
+++ src/Componentes/Checkout/Checkout.jsx
@@ -11,14 +11,54 @@ const Checkout = () => {
   const[telefono, setTelefono] = useState("");
   const[email, setEmail] = useState("");
   const[emailConfirmacion, setEmailConfirmacion] = useState("");
-  const[ordenId, setOrdenID] = useState("");
+  const[ordenId, setOrdenId] = useState("");
   const[error, setError] = useState("");
   
+    const manejadorSubmit = (event) =>{
+        if(!nombre || !apellido || !telefono || !email ||!emailConfirmacion) {
+            setError ("Por favor completar todos los campos");
+            return ;
+        }
+          if (email !== emailConfirmacion) {
+        setError("Los Email no coinciden");
+          }
+
+          const orden = {
+            items: carrito.map(producto => ({
+                id: producto.item.id,
+                nombre: producto.item.nombre,
+                cantidad: producto.cantidad
+            })),
+            total: total,
+            fecha: new Date(),
+            nombre,
+            apellido,
+            telefono,
+            email,
+          }
+
+          addDoc(collection(db, "ordenes"), orden)
+            .then(docRef => {
+                setOrdenId(docRef.id);
+                vaciarCarrito();
+            })
+            .catch(error =>{
+                console.log("Error al crear la orden de compra", error);
+                setError("Por favor intente mas tarde");
+            })
+
+
+
+        }   
+
+   
+
+
   return (
     <div>
         <h2>Checkout - Finalizar la compra </h2>
 
-        <form>
+        <form onSubmit={manejadorSubmit}>
             {
             carrito.map(producto => (
                 <div key={producto.item.id}>
@@ -52,6 +92,15 @@ const Checkout = () => {
             <label htmlFor="emailcon"> Confirmacion de Mail </label>
             <input type="text" id="email" onChange={(e)=> setEmailConfirmacion(e.target.value)}/>
            </div>
+
+        {
+            error && <p style={{color: "red"}}> {error} </p>
+        }
+
+        <button> Finalizar Orden </button>
+        {
+            ordenId && <strong> Gracias por su compra! Tú numero de orden es el siguiente: {ordenId} </strong>
+        }
 
         </form>
         </div>
